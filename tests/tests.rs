83a641868154c86dd2795896464c0c9c22d2069d
Lucas Rooyakkers <lucas.rooyakkers@protonmail.com>
2021-07-27 23:58:32 -0400

    Fixed up tests.

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,13 +1,10 @@
-extern crate rand;
-extern crate ping;
+extern crate tiny_ping;
 
 use std::time::Duration;
-
-use rand::random;
 
 #[test]
 fn basic() {
     let addr = "127.0.0.1".parse().unwrap();
     let timeout = Duration::from_secs(1);
-    ping::ping(addr, Some(timeout), Some(166), Some(3), Some(5), Some(&random())).unwrap();
+    tiny_ping::ping(addr, Some(timeout), Some(166), Some(3), Some(5), None).unwrap();
 }
